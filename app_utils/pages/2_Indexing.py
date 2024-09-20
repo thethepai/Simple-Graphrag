3a65db9a7943a620bdf4b7ef19e7c3aff0b1b21c
jack <lsj13960546483@outlook.com>
2024-09-20 20:38:53 +0800

    index page

--- app_utils/pages/2_Indexing.py
+++ app_utils/pages/2_Indexing.py
@@ -1,38 +1,52 @@
 import streamlit as st
 import os
+from api_utils.rag_client_init import InitPipeline
+from api_utils.default_config import DATA_INPUT_DIR, DATA_INDEX_DIR
 
 st.set_page_config(page_title="Indexing", page_icon="📊")
 
 st.markdown("# Indexing Configuration")
 st.sidebar.header("Indexing Configuration")
-st.write(
-    """Here you can configure the settings for the indexing."""
-)
+st.write("""Here you can choose files and do the indexing.""")
 
-save_folder = "uploaded_files"
+pipeline = InitPipeline()
+
+save_folder = f"{DATA_INPUT_DIR}"
 if not os.path.exists(save_folder):
     os.makedirs(save_folder)
 
-uploaded_files = st.file_uploader(
-    "Choose a txt or xlsx file", accept_multiple_files=True
-)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
+ragtest_input = f"{DATA_INDEX_DIR}"
+
+@st.cache
+def get_files(uploaded_files):
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        st.write("filename:", uploaded_file.name)
+
+        with open(os.path.join(save_folder, uploaded_file.name), "wb") as f:
+            f.write(bytes_data)
+        st.success(f"File saved to {os.path.join(save_folder, uploaded_file.name)}")
+
+        with open(os.path.join(ragtest_input, uploaded_file.name), "wb") as f:
+            f.write(bytes_data)
+        st.success(f"File saved to {os.path.join(ragtest_input, uploaded_file.name)}")
+
+
+if not os.path.exists(f"{pipeline.root_dir}"):
+    st.markdown("## Do the index init first and then use indexing")
+else:
+    uploaded_files = st.file_uploader(
+        "Choose a txt or xlsx file", accept_multiple_files=True
+    )
     
-    with open(os.path.join(save_folder, uploaded_file.name), "wb") as f:
-        f.write(bytes_data)
-    st.write(f"File saved to {os.path.join(save_folder, uploaded_file.name)}")
+    get_files(uploaded_files)
 
-
-def start_index():
-    pass
-
-def prompt_tune():
-    pass
 
 if st.sidebar.button("Start Indexing"):
-    start_index()
+    if os.listdir(DATA_INDEX_DIR):
+        pipeline.default_start_index()
+    else:
+        st.sidebar.error("Please upload files first")
 
 if st.sidebar.button("Prompt Tune"):
-    prompt_tune()
+    st.sidebar.warning("This feature is not available yet")
